c723e584ccb6989a721290a4b1ad0526ed0ff6ea
devdaeun <ekdms008@gmail.com>
2025-04-01 12:19:15 +0900

    게시글 구현: 게시글 entity 데이터베이스 연동 확인

--- src/main/java/com/notice/notice_board/domain/model/Post.java
+++ src/main/java/com/notice/notice_board/domain/model/Post.java
@@ -1,19 +1,31 @@
 package com.notice.notice_board.domain.model;
 
 import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
+@Getter
+@Table(name = "p_posts")
+@NoArgsConstructor
 public class Post extends Default{
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "post_id", nullable = false)
-    private int Id;
+    private Long Id;
 
     @Column(nullable = false)
     private String title;
 
     @Column(nullable = false)
     private String content;
+
+    @Builder
+    public Post(String title, String content){
+        this.title = title;
+        this.content = content;
+    }
 
 }
