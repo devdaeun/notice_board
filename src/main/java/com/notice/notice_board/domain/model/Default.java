faa0cf3952903c9608b29c4b5a16c749c3b8740c
devdaeun <ekdms008@gmail.com>
2025-04-01 15:26:38 +0900

    게시글 구현: soft delete 구현 어노테이션 오류 수정

--- src/main/java/com/notice/notice_board/domain/model/Default.java
+++ src/main/java/com/notice/notice_board/domain/model/Default.java
@@ -2,12 +2,13 @@ package com.notice.notice_board.domain.model;
 
 import jakarta.persistence.*;
 import org.hibernate.annotations.SoftDelete;
+import org.hibernate.type.TrueFalseConverter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.util.Date;
-
+@SoftDelete(converter = TrueFalseConverter.class, columnName = "is_deleted")
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
 public class Default {
@@ -21,8 +22,6 @@ public class Default {
     @Column(name = "updated_at")
     @Temporal(TemporalType.TIMESTAMP)
     private Date updatedAt;
-
-    @SoftDelete(columnName = "is_deleted") //soft delete 어노테이션
 
     @Column(name = "is_deleted", insertable = false, updatable = false)
     private boolean isDeleted;
